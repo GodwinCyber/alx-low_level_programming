b6c262711421399774ae16843297f42b754766d2
GodwinCyber <godwinchuks032@gmail.com>
2023-08-10 11:41:45 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 	float f;
 	printf("size of a char: %zu byte(s)\n",(unsigned long)sizeof(d));
 	printf("size of an int: %zu byte(s)\n",(unsigned long)sizeof(a));
-	printf("size of a int long: %zu byte(s)\n",(unsigned long)sizeof(b));
-	printf("size of a int long long: %zu byte(s)\n",(unsigned long)sizeof(c));
+	printf("size of a long int: %zu byte(s)\n",(unsigned long)sizeof(b));
+	printf("size of a long long int: %zu byte(s)\n",(unsigned long)sizeof(c));
 	printf("size of a float: %zu byte(s)\n",(unsigned long)sizeof(f));
 	return (0);
 }
