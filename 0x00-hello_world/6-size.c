3827b125cac70f505c5a5146bb23665113d7fb5d
GodwinCyber <godwinchuks032@gmail.com>
2023-08-10 11:38:51 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	printf("size of a char: %zu byte(s)\n",(unsigned long)sizeof(d));
+	printf("size of an int: %zu byte(s)\n",(unsigned long)sizeof(a));
+	printf("size of a int long: %zu byte(s)\n",(unsigned long)sizeof(b));
+	printf("size of a int long long: %zu byte(s)\n",(unsigned long)sizeof(c));
+	printf("size of a float: %zu byte(s)\n",(unsigned long)sizeof(f));
+	return (0);
+}
