a98279bd70b4ae33b32c54eea148ad5382a444a4
GodwinCyber <godwinchuks032@gmail.com>
2023-08-11 20:49:13 -0700

    print alphabetic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print the alphabetic
+ * Retun: Always
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
