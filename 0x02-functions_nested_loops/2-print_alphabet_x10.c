42bc6c9a9f86f69c02fb222e308a6a22fcfcf25d
GodwinCyber <godwinchuks032@gmail.com>
2023-08-15 10:29:01 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 0)
+	while (i <=10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar (c);
-		}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar (c);
 	}
-	{
-		_putchar ('\n');
-		i++;
+	_putchar ('\n');
+	i++;
 	}
 }
