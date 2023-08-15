0133b64ebb909f6ce2e830dd6c7646c24c02a5d5
GodwinCyber <godwinchuks032@gmail.com>
2023-08-15 12:54:42 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,11 +23,12 @@ void times_table(void)
 			}
 			else
 			{
-				if (product < 0)
+				if (product < 10)
 				{
 					_putchar (' ');
 				}
-				_putchar (product / 10 + '0');
+				_putchar (product / 100 + '0');
+				_putchar (product / 10 % 10 + '0');
 				_putchar (product % 10 + '0');
 			}
 
