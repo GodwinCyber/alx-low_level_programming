ea29ddfd6e1ca893bfe43d81503f7af605f3bf45
GodwinCyber <godwinchuks032@gmail.com>
2023-08-15 14:10:07 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,36 +8,37 @@
 
 void times_table(void)
 {
-	int i;
-	int j;
-	int product;
-	{
-		for (i = 0; i <= 9; i++)
-		{
-			for (j = 0; j <= 9; j++)
-				product = i * j;
+	int x, y, z, u, d;
 
-			if (j == 0)
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
 			{
-				_putchar ('0');
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
-				if (product < 10)
+				if (y != 0)
 				{
-					_putchar (' ');
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
 				}
-				_putchar (product / 100 + '0');
-				_putchar (product / 10 % 10 + '0');
-				_putchar (product % 10 + '0');
-			}
 
-			if (j < 9)
-			{
-				_putchar (',');
-				_putchar (' ');
+				_putchar(z + '0');
 			}
 		}
-		_putchar ('\n');
+
+		_putchar('\n');
 	}
 }
