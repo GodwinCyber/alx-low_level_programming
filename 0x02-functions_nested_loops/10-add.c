93fa60a723bd93c7a08b6edcc449b90a20d0e82c
GodwinCyber <godwinchuks032@gmail.com>
2023-08-15 14:22:20 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result
+ * @a: stdout
+ * @b: stdout
+ * Return: Always (Success)
+ *
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
