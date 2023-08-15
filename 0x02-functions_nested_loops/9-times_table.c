ff673033b444607b4e70eca39ffbfb22445e72e9
GodwinCyber <godwinchuks032@gmail.com>
2023-08-15 12:47:33 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: Always (Success)
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			for (j = 0; j <= 9; j++)
+				product = i * j;
+
+			if (j == 0)
+			{
+				_putchar ('0');
+			}
+			else
+			{
+				if (product < 0)
+				{
+					_putchar (' ');
+				}
+				_putchar (product / 10 + '0');
+				_putchar (product % 10 + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+		_putchar ('\n');
+	}
+}
