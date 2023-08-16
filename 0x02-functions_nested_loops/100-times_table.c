e6da34537a42969506d0c5c837a45559d1716109
GodwinCyber <godwinchuks032@gmail.com>
2023-08-15 19:47:54 -0700

    print alphabet

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,46 @@
+/*
+ * File: 100-times_table.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 
 /**
- * print_times_table -  print times table, starting with.
- *
- * @n: n number parameter
- * Return: Always (Success)
+ * print_times_table - Prints the times table of the input
+ * @n: The value of the times table to be printed.
  */
-
 void print_times_table(int n)
 {
-	int a, b, c;
+	int num, mult, prod;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (num = 0; num <= n; num++)
 		{
-			for (b = 0; b <= n; b++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				c = a * b;
-				if (c > 99)
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((c / 100) + '0');
-					_putchar(((c / 10) % 10) + '0');
-					_putchar((c % 10) + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (c > 9)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((c / 10) % 10) + '0');
-					_putchar((c % 10) + '0');
+					_putchar((prod / 10) + '0');
 				}
-				else
-				{
-					if (b != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(c + '0');
-				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
