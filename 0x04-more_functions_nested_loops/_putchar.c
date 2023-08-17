4648464f2683e22402d436adf09f2296ad45c10c
GodwinCyber <godwinchuks032@gmail.com>
2023-08-17 09:16:03 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Write the character to the standard out srdout
+ * @c: The charcater to be writen, write the c character to the stdout
+ * @c: The character to be printed out; parameter
+ *
+ * Return: On Sucess 1
+ * On error: -1 and set the error appropriate
+ */
+
+int _putchar (char c)
+{
+	return write(1, &c, 1);
+}
