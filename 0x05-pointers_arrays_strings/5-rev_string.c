d404c0eef3fd97303e3030c50d35e7982dbcf32a
GodwinCyber <godwinchuks032@gmail.com>
2023-08-21 12:51:12 -0700

    print reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse the string
+ * @s: count the string length
+ * Return: swap character to revere the string
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
