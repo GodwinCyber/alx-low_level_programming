41b1a5b19cce5204487d513f16ac51ecb25caa46
GodwinCyber <godwinchuks032@gmail.com>
2023-08-21 13:59:50 -0700

    prints every other character of a string,

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: Always Success
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+
+	puts2(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts -  prints every other character of a string,
+ * starting with the first character,
+ * @str: string to printed
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index += 2;
+	}
+	_putchar('\n');
+}
