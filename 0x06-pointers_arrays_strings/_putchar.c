be11725b6f910291edd071453a63ee3a6b617f7c
GodwinCyber <godwinchuks032@gmail.com>
2023-08-23 05:41:46 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write character to the standard output, stdout
+ * @c: charater to be written, write in the stdout.
+ * @: character to be printed out: parameter
+ * Return: Always success 1
+ * Error: -1, set the error appropraite
+ */
+
+int _putchar(char c)
+{
+	return write (1, $c, 1);
+}
