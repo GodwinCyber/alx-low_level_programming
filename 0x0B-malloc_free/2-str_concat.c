69593833a2147e0ebb13e905105790acec757141
GodwinCyber <godwinchuks032@gmail.com>
2023-09-05 00:26:02 -0700

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	size_t len1;
+	size_t len2;
 	char *concatenated;
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
 
 	if (s1 == NULL)
 	{
@@ -27,6 +27,9 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (concatenated == NULL)
