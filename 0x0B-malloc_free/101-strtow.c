0922b57c528b491523f6e10d487c7a4a5d73984f
GodwinCyber <godwinchuks032@gmail.com>
2023-09-05 20:12:02 -0700

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,65 +1,5 @@
 #include "main.h"
 
-char **strtow(char *str);
-int countWords(char *str);
-char *copyWord(char *str, int start, int end);
-
-/**
- * countWords - count the number of words in
- * strings argunment.
- * @str: the input string to be counted
- * Return: the number of words in input strings
- */
-
-int countWords(char *str)
-{
-	int count = 0;
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		while (str[i] == ' ')
-		{
-			i++;
-		}
-		if (str[i] != '\0')
-		{
-			count++;
-		}
-		while (str[i] != ' ' && str[i] != '\0')
-		{
-			i++;
-		}
-	}
-	return (count);
-}
-
-/**
- *  copyWord - allocate memory to the words and copy it
- *  @str: The input string containing the word
- *  @start: The starting index of the word within the input string
- *  @end: The ending index of the word within the input string
- *  Return: A dynamically allocated string containing the copied word
- */
-
-char *copyWord(char *str, int start, int end)
-{
-	int length = end - start;
-	char *word = (char *)malloc((length + 1) * sizeof(char));
-	int i;
-
-	if (word == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < length; i++)
-	{
-		word[i] = str[start + i];
-	}
-	word[length] = '\0';
-	return (word);
-}
-
 /**
  * strtow - Split a string into words
  * @str: The input string to be split
@@ -68,54 +8,22 @@ char *copyWord(char *str, int start, int end)
 
 char **strtow(char *str)
 {
-	char **words;
-	int i, wordIndex, wordStart, wordCount, j;
+	char *array = NULL;
+	unsigned int i = 0, j = 0, a;
 
-	if (str == NULL || *str == '\0')
-	{
+	if (strncmp(str, "", 1) || str == NULL)
 		return (NULL);
-	}
-	wordCount = countWords(str);
-	words = (char **)malloc((wordCount + 1) * sizeof(char *));
-
-	if (words == NULL)
-	{
+	array = malloc((i + j + 1) * sizeof(char));
+	if (array == NULL)
 		return (NULL);
-	}
-	i = 0;
-	wordIndex = 0;
-	wordStart = -1;
-
-	while (str[i] != '\0')
+	for (a = 0; a < i; a++)
+		array[a] = str[a];
+	i = a;
+	for (a = 0; a < j; a++)
 	{
-		while (str[i] == ' ')
-		{
-			i++;
-		}
-		if (str[i] != '\0')
-		{
-			wordStart = i;
-		}
-		while (str[i] != ' ' && str[i] != '\0')
-		{
-			i++;
-		}
-		if (wordStart != -1)
-		{
-			words[wordIndex] = copyWord(str, wordStart, i);
-			if (words[wordIndex] == NULL)
-			{
-				for (j = 0; j < wordIndex; j++)
-				{
-					free(words[j]);
-				}
-				free(words);
-				return (NULL);
-			}
-			wordIndex++;
-			wordStart = -1;
-		}
+		array[i] = str[a];
+		i++;
 	}
-	words[wordCount] = NULL;
-	return (words);
+	array[i] = '\0';
+	return (NULL);
 }
