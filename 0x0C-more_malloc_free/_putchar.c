8a9bf7d4a83d527b2c0256e7b39da8bc65a90875
GodwinCyber <godwinchuks032@gmail.com>
2023-09-07 05:23:04 -0700

    The header/putchar

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - write the character to the stdout
+ * @c: the single arunment parameter
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	return write (1, &c, 1);
+}
