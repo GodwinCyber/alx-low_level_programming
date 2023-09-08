ea5e045e6c208516be31cecd65a268b0b68ebf74
GodwinCyber <godwinchuks032@gmail.com>
2023-09-07 17:57:39 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * is_positive_number - check if a string contains only positive numbers
+ * @str: the input string
+ * Return: Return 0 if a non-digit character is found
+ */
+
 int is_positive_number(char *str)
 {
 	while (*str)
@@ -14,6 +20,13 @@ int is_positive_number(char *str)
 	}
 	return (1);
 }
+
+/**
+ * multiply - multiply two positive numbers represented as strings
+ * @num1: string one
+ * @num2: second string
+ * Return: 0
+ */
 
 void multiply(char *num1, char *num2)
 {
@@ -30,7 +43,6 @@ void multiply(char *num1, char *num2)
 		len2++;
 	}
 	result = (int *)calloc(len1 + len2, sizeof(int));
-	
 	for (i = len1 - 1; i >= 0; i--)
 	{
 		for (j = len2 - 1; j >= 0; j--)
@@ -51,9 +63,17 @@ void multiply(char *num1, char *num2)
 		printf("%d", result[i]);
 	}
 	printf("\n");
-	
 	free(result);
 }
+
+/**
+ * main - Check if the correct number of arguments is provided and if
+ * they are positive numbers
+ * @argv: Array of strings, where each element is a command-line argument
+ * @argc:Argument count, representing the number of command-line arguments
+ * Return: Return 98 to indicate an error
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc != 3 || !is_positive_number(argv[1]) || !is_positive_number(argv[2]))
@@ -62,6 +82,5 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 	multiply(argv[1], argv[2]);
-	
 	return (0);
 }
