184d34ca2168a7dc7a957dd7503572e385661db8
GodwinCyber <godwinchuks032@gmail.com>
2023-09-14 08:10:50 -0700

    prints strings, followed by a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,13 +11,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list arg;
 	unsigned int i = 0;
-	const char *str;
+	char *str;
 
 	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(arg, const char *);
+		str = va_arg(arg, char *);
 
 		if (str == NULL)
 			printf("nil");
