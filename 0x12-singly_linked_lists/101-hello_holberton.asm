39bb2e50cac17736f57e77740ed1cfc067f40278
GodwinCyber <godwinchuks032@gmail.com>
2023-09-21 16:36:28 -0700

    Write a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,17 @@
 section .data
-    hello db "Hello, Holberton",10 ; Message to be printed, followed by a newline character
+    hello db "Hello, Holberton",10  ; Message to be printed, followed by a newline character
+    format db "%s",0              ; Format string for printf, null-terminated
 
 section .text
     global main
     extern printf
 
 main:
-    sub rsp, 8             ; Align the stack
-    mov rdi, hello         ; Load the format string
-    call printf            ; Call the printf function
-    add rsp, 8             ; Restore the stack
+    push rbp               ; Save the base pointer
+    mov rdi, format       ; Load the format string into rdi
+    mov rsi, hello        ; Load the address of the message into rsi
+    call printf           ; Call printf
+    pop rbp                ; Restore the base pointer
 
     ; Exit the program
     mov rax, 60            ; syscall number for exit
