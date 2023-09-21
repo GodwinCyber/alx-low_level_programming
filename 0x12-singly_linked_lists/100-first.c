52426cdefa3212a1e55ccf4754ea510322c6891f
GodwinCyber <godwinchuks032@gmail.com>
2023-09-21 15:30:17 -0700

    wtite a function that print before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
+void before_main(void) __attribute__((constructor));
+
 /**
- * beforee_main - write a function that print before
+ * before_main - write a function that print before
  * the main function is executed
  * @__attibute__((constructor)): This is a GCC attribute
  * that tell the compiler to execute before_main before the main
@@ -9,10 +11,8 @@
  * Return: 0
  */
 
-
-void before_main(void) __attribute__((constructor));
-
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
