8c6ecb7a59e53112bbeb5ad7b702ce97ecea3225
GodwinCyber <godwinchuks032@gmail.com>
2023-09-28 08:44:50 -0700

    converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <strarg.h>
+#include <stdarg.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
