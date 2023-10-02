b95277d1e6217a9f0422c6e72e7ca09635f646f3
GodwinCyber <godwinchuks032@gmail.com>
2023-10-02 15:51:24 -0700

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,11 @@
 #include "main.h"
+#include <errno.h>
+
+
+int open_source_file(const char *filename);
+int open_target_file(const char *filename);
+void copy_file(int source_fd, int target_fd);
+void close_file(int fd);
 
 /**
  * main - Copies the content of a file to another file.
@@ -10,33 +17,102 @@
 
 int main(int ac, char **av)
 {
-	int src_fd, dest_fd;
-	ssize_t bytes;
-	char buffer[BUFFER_SIZE];
+	int source_fd, target_fd;
 
 	if (ac != 3)
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]), exit(97);
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
 
-	src_fd = open(av[1], O_RDONLY);
-	if (src_fd == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+	source_fd = open_source_file(av[1]);
+	target_fd = open_target_file(av[2]);
 
-	dest_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (dest_fd == -1)
-		dprintf(2, "Error: Can't write to file %s\n", av[2]),
-			close(src_fd), exit(99);
+	copy_file(source_fd, target_fd);
 
-	while ((bytes = read(src_fd, buffer, BUFFER_SIZE)) > 0)
-		if (write(dest_fd, buffer, bytes) == -1)
-			dprintf(2, "Error: Can't write to file %s\n", av[2]),
-				close(src_fd), close(dest_fd), exit(99);
-
-	if (bytes == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]),
-			close(src_fd), close(dest_fd), exit(98);
-
-	if (close(src_fd) == -1 || close(dest_fd) == -1)
-		dprintf(2, "Error: Can't close fd\n"), exit(100);
+	close_file(source_fd);
+	close_file(target_fd);
 
 	return (0);
+}
+
+/**
+ * open_source_file - Opens the source file for reading.
+ * @filename: The name of the source file to open.
+ *
+ * Return: The file descriptor of the opened file.
+ */
+
+int open_source_file(const char *filename)
+{
+	int source_fd = open(filename, O_RDONLY);
+
+	if (source_fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (source_fd);
+}
+
+/**
+ * open_target_file - Opens the target file for writing.
+ * @filename: The name of the target file to open.
+ *
+ * Return: The file descriptor of the opened file.
+ */
+
+int open_target_file(const char *filename)
+{
+	int target_fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	if (target_fd == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", filename);
+		exit(99);
+	}
+	return (target_fd);
+}
+
+/**
+ * copy_file - Copies the content from the source file to the target file.
+ * @source_fd: The file descriptor of the source file.
+ * @target_fd: The file descriptor of the target file.
+ */
+
+void copy_file(int source_fd, int target_fd)
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(target_fd, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(2, "Error: Can't write to file\n");
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file\n");
+		exit(98);
+	}
+}
+
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to close.
+ */
+
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }
