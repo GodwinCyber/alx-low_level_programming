08b3f5075490f6fe9464757f42282bf4546c3ef2
GodwinCyber <godwinchuks032@gmail.com>
2023-11-09 20:06:44 -0800

    commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	for (; h; h = h->next)
 	{
-		if (h->prev != NULL)
-		{
-			printf("-> ");
-		}
+		n++;
 		printf("%d\n", h->n);
-		h = h->next;
-		count++;
 	}
-	return (count);
+
+	return (n);
 }
