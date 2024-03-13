4582cd9a0621ac8cf94bb7d89153e837c6d4aceb
GodwinCyber <godwinchuks032@gmail.com>
2024-03-13 16:36:55 -0700

    searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 
-size_t min(size_t a, size_t b);
 int _binary_search(int *array, size_t left, size_t right, int value);
 
 /**
@@ -12,28 +11,23 @@ int _binary_search(int *array, size_t left, size_t right, int value);
  * Return: The first index where value is located, or -1
  * if value is not present or array is NULL.
  */
+
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t bound = 1;
-	size_t left, right;
-	int result;
+	size_t bound = 0, right;
 
 	if (array == NULL)
 		return (-1);
 
-	if (array[0] == value)
-		return (0);
-
-	while (bound < size && array[bound] < value)
+	if (array[0] != value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
-		bound *= 2;
+		for (bound = 1; bound < size && array[bound] <= value; bound = bound * 2)
+			printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
 	}
-	left = bound / 2;
-	right = min(bound, size - 1);
-	printf("Value found between indexes [%ld] and [%ld]\n", left, right);
-	result = _binary_search(array + left, left, right, value);
-	return (result == -1 ? -1 : result + (int)left);
+
+	right = bound < size ? bound : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", bound / 2, right);
+	return (_binary_search(array, bound / 2, right, value));
 }
 
 /**
@@ -43,7 +37,7 @@ int exponential_search(int *array, size_t size, int value)
  * @left: The first index of the range to search in.
  * @right: The last index of the range to search in.
  * @value: The value to search for.
- * Return: The first index where value is located, or -1 
+ * Return: The first index where value is located, or -1
  * if value is not present.
  */
 int _binary_search(int *array, size_t left, size_t right, int value)
@@ -67,15 +61,4 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 	}
 
 	return (-1);
-}
-
-/**
- * min - Utility function to find the minimum of two numbers.
- * @a: First number.
- * @b: Second number.
- * Return: The smaller of the two numbers.
- */
-size_t min(size_t a, size_t b)
-{
-	return ((a < b) ? a : b);
 }
