a5af0b812ab4cf3b504dbc1871cfd9770a1ef0c7
GodwinCyber <godwinchuks032@gmail.com>
2024-03-13 15:51:59 -0700

    searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,14 +1,6 @@
 #include "search_algos.h"
-/**
- * min - Utility function to find the minimum of two numbers
- * @a: First number.
- * @b: Second number.
- * Return: The smaller of the two number
- */
-size_t min(size_t a, size_t b)
-{
-	return ((a < b) ? a : b);
-}
+
+size_t min(size_t a, size_t b);
 
 /**
  * jump_search -  searches for a value in a sorted array of integers
@@ -21,24 +13,33 @@ size_t min(size_t a, size_t b)
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jump = sqrt(size), prev, step;
+	size_t step, prev, jump = sqrt(size);
 
 	if (!array || size == 0)
 		return (-1);
 
 	for (prev = 0; prev < size && array[prev] < value;
-			step = prev, prev += jump)
+	     step = prev, prev += jump)
 	{
 		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n",
-			prev, min(step, size));
+	printf("Value found between indexes [%ld] and [%ld]\n", step, prev);
 
 	for (; step <= min(prev, size - 1); step++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
+		printf("Value checked array[%ld] = [%d]\n", step, array[step]);
+		if (array[step] == value)
+			return (step);
 	}
 	return (-1);
+}
+/**
+ * min - Utility function to find the minimum of two numbers
+ * @a: First number.
+ * @b: Second number.
+ * Return: The smaller of the two number
+ */
+size_t min(size_t a, size_t b)
+{
+	return ((a < b) ? a : b);
 }
